2c2c749cf20b3297b5f6b75fc7d2e4b1c96d5d2c
guna144 <gunaapps14@gmail.com>
2018-10-24 11:37:33 +0530

    adding readFile vs createStream

---
+++ read-file-vs-stream/create-csv-file.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const cols = 9;
+
+function randomChar(){
+	return String.fromCharCode(Math.floor(Math.random() * (122-65) + 65));
+}
+
+function randomString(length){
+	let string = '';
+	for(let i = 0; i < length; i++){
+		string += randomChar();
+	}
+	return string;
+}
+
+function randomRow(){
+	const cells = [];
+	for(let i = 0; i < cols; i++){
+		cells.push(randomString(20));
+	}
+	return cells.join(',') + '\n';
+}
+
+async function writeRow(stream){
+	return new Promise((resolve, reject)=> {
+		stream.write(randomRow(), 'utf8', () => resolve());
+	});
+}
+
+async function createCSVs(sizes){
+	sizes.forEach(async size => {
+		const stream = fs.createWriteStream(`dummy${size}.csv`);
+		for(let i = 0; i < size; i++){
+			await writeRow(stream);
+		}
+		stream.end();
+	});
+}
+
+createCSVs([1000,10000,100000,1000000]);
+
+//https://medium.com/@dalaidunc/fs-readfile-vs-streams-to-read-text-files-in-node-js-5dd0710c80ea
